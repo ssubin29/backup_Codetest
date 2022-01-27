f539c388bc2558a63db5582f21c630537a89ae9f
ssubin29 <20201003@sungshin.ac.kr>
2022-01-27 23:02:22 +0900

    Update ch12_35.py

--- ch12/ch12_35.py
+++ ch12/ch12_35.py
@@ -21,3 +21,4 @@ class Solution:
         return list(itertools.combinations(range(1, n + 1), k))
 
 print(Solution().combine2(5,4))
+# [(1, 2, 3, 4), (1, 2, 3, 5), (1, 2, 4, 5), (1, 3, 4, 5), (2, 3, 4, 5)]
