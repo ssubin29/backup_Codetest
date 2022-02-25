78f4fc8abd084b80cccfb35a03dc26ced05133fd
ssubin29 <20201003@sungshin.ac.kr>
2022-02-26 02:17:02 +0900

    3-1 3-2 3-3

---
+++ Ch3_Greedy/3-2.py
@@ -0,0 +1,17 @@
+n,m,k = map(int, input().split())
+
+data = list(map(int,input().split()))
+
+data.sort(reverse=True)
+first = data[0]
+second = data[1]
+
+# 가장 큰 수가 더해지는 횟수 계산
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result = 0
+result += (count) * first # 가장 큰 수 더하기
+result += (m - count) * second # 두 번째로 큰 수 더하기
+
+print(result) # 최종 답안 출력
