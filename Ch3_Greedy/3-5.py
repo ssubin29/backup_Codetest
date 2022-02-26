79bd98f6af6ccd7ab1b021fad4ce8f50a0e786df
ssubin29 <20201003@sungshin.ac.kr>
2022-02-27 01:18:17 +0900

    Update 3-5.py

--- Ch3_Greedy/3-5.py
+++ Ch3_Greedy/3-5.py
@@ -1,7 +1,6 @@
 # N, K을 공백을 기준으로 구분하여 입력 받기
 n, k = map(int, input().split())
 result = 0
-
 # N이 K 이상이라면 K로 계속 나누기
 while n >= k:
     # N이 K로 나누어 떨어지지 않는다면 N에서 1씩 빼기
@@ -11,7 +10,6 @@ while n >= k:
     # K로 나누기
     n //= k
     result += 1
-
 # 마지막으로 남은 수에 대하여 1씩 빼기
 while n > 1:
     n -= 1
